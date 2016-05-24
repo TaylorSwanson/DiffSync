99b7ad0d89d7cdc2dc849418cd631c4f5a38f2e4
Taylor Swanson <kaleidoflowers@gmail.com>
2016-05-24 16:50:35 -0600

    Made main LibOT interface prototypal

--- server/index.js
+++ server/index.js
@@ -7,16 +7,16 @@ var documentManager = require("../modules/DocumentManager.js");
 
 function LibOT(dataProvider) {
     this.dataProvider = dataProvider;
+};
 
-    // Get a client instance attached to the specified document
-    getClient: function getClient(id, callback) {
-        if (documentManager.isOpen(id)) {
-            return callback(null, documentManager.get(id).getClient());
-        }
-        documentManager.create(this.dataProvider, id, function(err, document) {
-            callback(err, document ? document.getClient() : null);
-        });
+// Get a client instance attached to the specified document
+LibOT.prototype.getClient = function getClient(id, callback) {
+    if (documentManager.isOpen(id)) {
+        return callback(null, documentManager.get(id).getClient());
     }
+    documentManager.create(this.dataProvider, id, function(err, document) {
+        callback(err, document ? document.getClient() : null);
+    });
 };
 
 module.exports = LibOT;
