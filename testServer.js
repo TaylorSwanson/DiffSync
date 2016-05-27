d33acf56569b6fa8a73d65dc99e1086287243a78
Taylor Swanson <kaleidoflowers@gmail.com>
2016-05-26 18:59:41 -0600

    Added test server

---
+++ testServer.js
@@ -0,0 +1,22 @@
+// var http = require("http");
+
+var serverFactory = require("spa-server");
+
+var server = serverFactory.create({
+    path: "./",
+    port: 80,
+    fallback: {
+        "text/javascript": "./OTClient.js",
+        "*": "index.html"
+    },
+    middleware: [
+        function (request, response, next) {
+            console.log(request.url);
+            // Don't allow browser to cache (this is the test server, after all)
+            response.setHeader("Cache-Control", "max-age=0, must-revalidate");
+            next();
+        }
+    ]
+});
+
+server.start();
